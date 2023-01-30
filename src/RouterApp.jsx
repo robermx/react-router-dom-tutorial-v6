f93dc3950aeac166fe798e4486faf9ff594bb3d7
Roberto Arenas <roberto.arenas2@icloud.com>
2023-01-29 21:28:33 -0600

    error page added

--- src/RouterApp.jsx
+++ src/RouterApp.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Root } from './routes/root';
+
+import { ErrorPage } from './ErrorPage';
+import { Root } from './routes/Root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
